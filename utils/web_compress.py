6782d2f00bfb820450026e35f444c6da1e044617
Sunset <26019675+lwd-temp@users.noreply.github.com>
2023-02-05 16:31:21 +0800

    补充注释

--- utils/web_compress.py
+++ utils/web_compress.py
@@ -1,13 +1,19 @@
 # 用于处理Web资源文件的降低质量
 # 在Web输出的game文件夹内执行
+# 图片压缩效果不佳
 import os
 
 from PIL import Image
 from pydub import AudioSegment
 
+# 配置
+# 压缩图像
 modify_image = False
+# 压缩音频
 modify_audio = True
+# 图片质量
 image_quality = 30
+# 音频质量
 audio_quality = 32000
 
 
